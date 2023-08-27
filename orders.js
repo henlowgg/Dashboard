1c161277a0afe9d2fad559fef9edd5235c471ee9
Jack Ault <henlowgg@outlook.com>
2023-08-27 19:11:12 -0400

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Milo Medical Bills',
+        productNumber: '00001',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Link Medical Bills',
+        productNumber: '00002',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Loml Medical Bills',
+        productNumber: '00003',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+]
